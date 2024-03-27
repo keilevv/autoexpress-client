56603a2b4b209e3e44ae0adb89807627301efe22
Caleb Villalba <calvihe@gmail.com>
2024-03-27 11:26:40 -0500

    authenticate user on main layout

--- src/hooks/useAuth.js
+++ src/hooks/useAuth.js
@@ -1,29 +1,51 @@
 import { useState, useCallback } from "react";
+import { useSelector } from "react-redux";
 import { useDispatch } from "react-redux";
-import { login, logout } from "../redux/reducers/authSlice"
+import { login, logout } from "../redux/reducers/authSlice";
+import { throwError } from "../helpers/index";
 import userService from "../services/user";
+import { useNavigate } from "react-router-dom";
 
 function useAuth() {
   const dispatch = useDispatch();
   const [loading, setLoading] = useState(false);
   const [user, setUser] = useState({});
+  const auth = useSelector((state) => state.auth);
+  const navigate = useNavigate();
 
+  const getUser = useCallback((userId) => {
+    setLoading(true);
+    return userService
+      .getUser(auth.user.accessToken, userId)
+      .then((response) => {
+        setUser(response.data);
+        setLoading(false);
+        return response;
+      })
+      .catch((err) => {
+        navigate("/login");
+      });
+  }, []);
 
   const loginUser = useCallback((username, password) => {
     setLoading(true);
-    return userService.login({ username, password })
+    return userService
+      .login({ username, password })
       .then((response) => {
         setUser(response.data);
-        dispatch(login(response.data))
+        dispatch(login(response.data));
         setLoading(false);
-        return response
+        return response;
       })
-  }, [])
+      .catch((err) => {
+        throwError(err.message.message);
+      });
+  }, []);
 
   function logoutUser() {
     setUser(null);
-    dispatch(logout())
+    dispatch(logout());
   }
-  return { user, loading, loginUser, logoutUser }
+  return { user, loading, loginUser, logoutUser, getUser };
 }
-export default useAuth
+export default useAuth;
