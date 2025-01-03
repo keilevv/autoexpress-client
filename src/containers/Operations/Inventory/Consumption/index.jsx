93f86107e3b8ff45a69d9ec0632a272b6fd67df6
keilevv <calvihe@gmail.com>
2025-01-03 17:39:03 -0500

    calculate inventory value

--- src/containers/Operations/Inventory/Consumption/index.jsx
+++ src/containers/Operations/Inventory/Consumption/index.jsx
@@ -1,11 +1,18 @@
+import { Skeleton } from "antd";
 import MaterialsTable from "../../../../components/operations/Inventory/MaterialsTable";
 import { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import useMaterials from "../../../../hooks/useInventory";
+import { formatToCurrency } from "../../../../helpers";
 
 function ConsumptionInventoryContainer({ refresh, searchValue, owner }) {
-  const { consumptionMaterials, getConsumptionMaterials, loading, count } =
-    useMaterials();
+  const {
+    consumptionMaterials,
+    getConsumptionMaterials,
+    loading,
+    count,
+    totalPriceConsumption,
+  } = useMaterials();
   const user = useSelector((state) => state.auth.user);
   const [pagination, setPagination] = useState({
     current: 1,
@@ -48,6 +55,18 @@ function ConsumptionInventoryContainer({ refresh, searchValue, owner }) {
 
   return (
     <div className="bg-gray-100 rounded-lg">
+      {loading ? (
+        <div className="flex flex-col gap-2 p-4">
+          <Skeleton.Input size="small" /> <Skeleton.Input size="small" />
+        </div>
+      ) : (
+        <div className="flex flex-col gap-2 p-4">
+          <p className="font-semibold text-base">{`Valor de consumo: ${formatToCurrency(
+            totalPriceConsumption
+          )}`}</p>
+          <p className="font-semibold"># de productos: {count}</p>
+        </div>
+      )}
       <MaterialsTable
         owner={owner}
         type={"consumption"}
